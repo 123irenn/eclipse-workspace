03ba164d9c70b2e210c1811812a51c3edbfe7c1c
123irenn <112928214+123irenn@users.noreply.github.com>
2022-09-20 15:39:41 +0530

    commit

--- Day20th/src/user/Rev.java
+++ Day20th/src/user/Rev.java
@@ -1,13 +1,34 @@
 package user;
 import java.util.*;
 public class Rev {
+	 public static String reverse(String str) {
+	        String rev = "";
+	        for(int i=str.length()-1;i>=0;i--) {
+	            rev += str.charAt(i);
+	        }
+	        return rev;
+	    }
 
 	public static void main(String[] args) {
-		String s;
-		char[] arr=s.toCharArray();
+		 Scanner sc = new Scanner(System.in);
+	        System.out.println("Enter the string : ");
+	        String str = sc.nextLine();
+	        String[] arr = str.split("((?=\\\\.)|(?<=\\\\.)|(?=\\\\s)|(?<=\\\\s))");
+	        //System.out.println(arr.length);
+	        //for(int i=0;i<arr.length;i++)
+	            //System.out.println(arr[i]);
+	        //String newstr = "";
+	        for(int i=0;i<arr.length;i++)
+	            System.out.print(reverse(arr[i]));
+	            //newstr += reverse(arr[i]);
+	        //System.out.println(newstr);
 		
+                
+            
+      
 		// TODO Auto-generated method stub
 
 	}
-
 }
+
+
